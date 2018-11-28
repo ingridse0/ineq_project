4bc09b1614d43c2d77ff0d7208cf871dd7e929bd
danros1123 <danyell@gmx.net>
2018-11-28 17:40:52 +0100

    moved _setup_dnk.R to DNK/R directory

--- reports/DNK/R/_setup_dnk.R
+++ reports/DNK/R/_setup_dnk.R
@@ -11,12 +11,12 @@ if(!exists(c("country", "year"))) {
 # Download data
 silc.p <- tbl(pg, "pp") %>%
   filter(pb020 %in% country & pb010 %in% year) %>%
-  select(pb020, pb030, pb040, pb150, py010g, py050n, px010, px030) %>%
+  select(pb010, pb020, pb030, pb040, pb150, py010g, py021g, py050g, py050n, py080g, py090g, py100g, py110g, py120g, py130g, py140g, px010, px030) %>%
   collect(n = Inf)
 
 silc.h <- tbl(pg, "hh") %>%
   filter(hb020 %in% country & hb010 %in% year) %>%
-  select(hb020, hb030, hy010, hx010) %>%
+  select(hb010, hb020, hb030, hy010, hy040g, hy050g, hy060g, hy070g, hy080g, hy090g, hy120g, hy130g, hy140g, hx010, hx040, hx050) %>%
   collect(n = Inf)
 
 silc.d <- tbl(pg, "dd") %>%
@@ -41,13 +41,17 @@ silc.pd <- left_join(silc.p, silc.d %>% select(id_h, db020, db090))
 
 silc.hd <- left_join(silc.h, silc.d)
 
+# 
+# # Create total personal income --------------------------------------------
+# 
+# # Find string "py" (i.e. income variables) for summing up total personal income. 
+# silc.pd <- silc.pd %>% 
+#   mutate(total.inc = rowSums(silc.pd[, grep("py", colnames(silc.pd))], 
+#                              na.rm = TRUE)) 
 
-# Create total personal income --------------------------------------------
+# replace NAs with 0
 
-# Find string "py" (i.e. income variables) for summing up total personal income. 
-silc.pd <- silc.pd %>% 
-  mutate(total.inc = rowSums(silc.pd[, grep("py", colnames(silc.pd))], 
-                             na.rm = TRUE)) 
+
 
 # Fin ---------------------------------------------------------------------
 
