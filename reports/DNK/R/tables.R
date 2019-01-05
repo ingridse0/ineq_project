8d15466c9b31f197e784d881aa27013b392a1788
danros1123 <danyell@gmx.net>
2019-01-05 15:30:55 +0100

    removed second year column from tables

--- reports/DNK/R/tables.R
+++ reports/DNK/R/tables.R
@@ -2,7 +2,7 @@
 
 # 80/20 quantile share ratio
 
-frame_8020 <- data.frame(year, i11_8020[2], i12_8020[2], i13_8020[2],
+frame_8020 <- data.frame(i11_8020[2], i12_8020[2], i13_8020[2],
                          i21_8020[2], i22_8020[2], i23_8020[2],
                          statbank_8020
 )
@@ -11,7 +11,7 @@ frame_8020 <- round(frame_8020, digits = 2)
 
 # At risk of poverty rate
 
-frame_arpr <- data.frame(year, i11_arpr[2], i12_arpr[2], i13_arpr[2],
+frame_arpr <- data.frame(i11_arpr[2], i12_arpr[2], i13_arpr[2],
                          i21_arpr[2], i22_arpr[2], i23_arpr[2],
                          statbank_arpr
 )
@@ -20,7 +20,7 @@ frame_arpr <- round(frame_arpr, digits = 2)
 
 # Gini
 
-frame_gini <- data.frame(year, i11_gini[2], i12_gini[2], i13_gini[2],
+frame_gini <- data.frame(i11_gini[2], i12_gini[2], i13_gini[2],
                          i21_gini[2], i22_gini[2], i23_gini[2],
                          statbank_gini
 )
@@ -29,7 +29,7 @@ frame_gini <- round(frame_gini, digits = 2)
 
 # Mean
 
-frame_mean <- data.frame(year, i11_mean[2], i12_mean[2], i13_mean[2],
+frame_mean <- data.frame(i11_mean[2], i12_mean[2], i13_mean[2],
                          i21_mean[2], i22_mean[2], i23_mean[2],
                          statbank_mean_pre_tax_a,
                          statbank_mean_pre_tax_b,
@@ -43,7 +43,7 @@ frame_mean <- round(frame_mean, digits = 2)
 
 # Median
 
-frame_median <- data.frame(year, i11_median[2], i12_median[2], i13_median[2],
+frame_median <- data.frame(i11_median[2], i12_median[2], i13_median[2],
                          i21_median[2], i22_median[2], i23_median[2],
                          statbank_median
 )
@@ -52,7 +52,7 @@ frame_median <- round(frame_median, digits = 2)
 
 # Top ten share
 
-frame_t10s <- data.frame(year, i11_t10s[2], i12_t10s[2], i13_t10s[2],
+frame_t10s <- data.frame(i11_t10s[2], i12_t10s[2], i13_t10s[2],
                            i21_t10s[2], i22_t10s[2], i23_t10s[2]
 )
 frame_t10s <- round(frame_t10s, digits = 2)
