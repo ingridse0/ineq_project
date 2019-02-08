9590902458613941193abca01f3416531cf49d2c
danros1123 <danyell@gmx.net>
2019-02-08 13:36:54 +0100

    replaced statbank with eurostat data in tables

---
+++ reports/DNK/R/5_1_eurostat_data.R
@@ -0,0 +1,16 @@
+# This script reads the eurostat data
+
+eurostat_mean <- read.csv("reports/DNK/tables/avg_inc_eurostat.csv")[2]
+
+eurostat_median <- read.csv("reports/DNK/tables/med_inc_eurostat.csv")[2]
+
+eurostat_gini_pretax <- read.csv("reports/DNK/tables/gini_pretax_eurostat.csv")[2]
+
+eurostat_gini <- read.csv("reports/DNK/tables/gini_eurostat.csv")[2]
+
+eurostat_qsr <- read.csv("reports/DNK/tables/qsr_eurostat.csv")[2]
+
+eurostat_t10s <- read.csv("reports/DNK/tables/t10s_eurostat.csv")[2]
+
+eurostat_arpr <- read.csv("reports/DNK/tables/arpr_eurostat.csv")[2]
+
--- reports/DNK/R/6_tables.R
+++ reports/DNK/R/6_tables.R
@@ -10,55 +10,63 @@
 
 frame_8020 <- data.frame(year, i11_8020[2], i12_8020[2], i13_8020[2],
                          i21_8020[2], i22_8020[2], i23_8020[2],
-                         statbank_8020
+                         eurostat_qsr
 )
 frame_8020 <- round(frame_8020, digits = 2)
+
+write.csv(frame_8020, "reports/DNK/tables/frame_8020.csv")
 
 
 # At risk of poverty rate
 
 frame_arpr <- data.frame(year, i11_arpr[2], i12_arpr[2], i13_arpr[2],
                          i21_arpr[2], i22_arpr[2], i23_arpr[2],
-                         statbank_arpr
+                         eurostat_arpr
 )
 frame_arpr <- round(frame_arpr, digits = 2)
+
+write.csv(frame_arpr, "reports/DNK/tables/frame_arpr.csv")
 
 
 # Gini
 
 frame_gini <- data.frame(year, i11_gini[2], i12_gini[2], i13_gini[2],
                          i21_gini[2], i22_gini[2], i23_gini[2],
-                         statbank_gini/100
+                         eurostat_gini_pretax, eurostat_gini
 )
 frame_gini <- round(frame_gini, digits = 2)
+
+write.csv(frame_gini, "reports/DNK/tables/frame_gini.csv")
 
 
 # Mean
 
 frame_mean <- data.frame(year, i11_mean[2], i12_mean[2], i13_mean[2],
                          i21_mean[2], i22_mean[2], i23_mean[2],
-                         statbank_mean_pre_tax_a,
-                         statbank_mean_pre_tax_b,
-                         statbank_mean_prim_a,
-                         statbank_mean_prim_b,
-                         statbank_mean_disp_a,
-                         statbank_mean_disp_b
+                         eurostat_mean
                          )
 frame_mean <- round(frame_mean, digits = 2)
 
+write.csv(frame_mean, "reports/DNK/tables/frame_mean.csv")
 
 # Median
 
-frame_median <- data.frame(year, i11_median[2], i12_median[2], i13_median[2],
+frame_median <- data.frame(i11_median[2], i12_median[2], i13_median[2],
                          i21_median[2], i22_median[2], i23_median[2],
-                         statbank_median
+                         eurostat_median
 )
 frame_median <- round(frame_median, digits = 2)
 
+write.csv(frame_median, "reports/DNK/tables/frame_median.csv")
 
 # Top ten share
 
 frame_t10s <- data.frame(year, i11_t10s[1], i12_t10s[1], i13_t10s[1],
-                           i21_t10s[1], i22_t10s[1], i23_t10s[1]
+                           i21_t10s[1], i22_t10s[1], i23_t10s[1],
+                         eurostat_t10s
 )
 frame_t10s <- round(frame_t10s, digits = 2)
+
+write.csv(frame_t10s, "reports/DNK/tables/frame_t10s.csv")
+
+
