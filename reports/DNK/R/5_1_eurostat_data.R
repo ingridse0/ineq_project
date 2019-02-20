69b4d96df9f3522ab19f03bb8f889f1765ebca7d
danros1123 <danyell@gmx.net>
2019-02-20 10:27:53 +0100

    Rounded gini in eurostat script

--- reports/DNK/R/5_1_eurostat_data.R
+++ reports/DNK/R/5_1_eurostat_data.R
@@ -7,6 +7,7 @@ eurostat_median <- read.csv("reports/DNK/tables/med_inc_eurostat.csv")[2]
 eurostat_gini_pretax <- read.csv("reports/DNK/tables/gini_pretax_eurostat.csv")[2]
 
 eurostat_gini <- read.csv("reports/DNK/tables/gini_eurostat.csv")[2]
+eurostat_gini <- round(eurostat_gini / 100, digits = 2)
 
 eurostat_qsr <- read.csv("reports/DNK/tables/qsr_eurostat.csv")[2]
 
