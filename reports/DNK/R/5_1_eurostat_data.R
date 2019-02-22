195a5c46f045aa8cf7d1a3572ec306618941e7fa
danros1123 <danyell@gmx.net>
2019-02-22 06:28:56 +0100

    fixed eurostat pretax gini

--- reports/DNK/R/5_1_eurostat_data.R
+++ reports/DNK/R/5_1_eurostat_data.R
@@ -5,6 +5,7 @@ eurostat_mean <- read.csv("reports/DNK/tables/avg_inc_eurostat.csv")[2]
 eurostat_median <- read.csv("reports/DNK/tables/med_inc_eurostat.csv")[2]
 
 eurostat_gini_pretax <- read.csv("reports/DNK/tables/gini_pretax_eurostat.csv")[2]
+eurostat_gini_pretax <- round(eurostat_gini_pretax / 100, digits = 2)
 
 eurostat_gini <- read.csv("reports/DNK/tables/gini_eurostat.csv")[2]
 eurostat_gini <- round(eurostat_gini / 100, digits = 2)
--- reports/DNK/R/8_tables_full_anhang.R
+++ reports/DNK/R/8_tables_full_anhang.R
@@ -31,8 +31,8 @@ name_vector <- c("Jahr",
 # 80/20 quantile share ratio
 
 frame_8020a <- data.frame(year, i11_8020[2], i12_8020[2], i13_8020[2],
-                         i21_8020[2], i22_8020[2], i23_8020[2],
-                         eurostat_qsr, row.names = NULL
+                          i21_8020[2], i22_8020[2], i23_8020[2],
+                          eurostat_qsr, row.names = NULL
 )
 frame_8020a <- round(frame_8020a, digits = 2)
 names(frame_8020a) <- name_vector
@@ -42,8 +42,8 @@ save(frame_8020a, file="reports/DNK/tables/frame_8020a.Rda")
 # At risk of poverty rate
 
 frame_arpra <- data.frame(year, i11_arpr[2], i12_arpr[2], i13_arpr[2],
-                         i21_arpr[2], i22_arpr[2], i23_arpr[2],
-                         eurostat_arpr, row.names = NULL
+                          i21_arpr[2], i22_arpr[2], i23_arpr[2],
+                          eurostat_arpr, row.names = NULL
 )
 frame_arpra <- round(frame_arpra, digits = 2)
 names(frame_arpra) <- name_vector
@@ -53,8 +53,8 @@ save(frame_arpra, file="reports/DNK/tables/frame_arpra.Rda")
 # Gini
 
 frame_ginia <- data.frame(year, i11_gini[2], i12_gini[2], i13_gini[2],
-                         i21_gini[2], i22_gini[2], i23_gini[2],
-                         eurostat_gini_pretax, eurostat_gini, row.names = NULL
+                          i21_gini[2], i22_gini[2], i23_gini[2],
+                          eurostat_gini_pretax, eurostat_gini, row.names = NULL
 )
 frame_ginia <- round(frame_ginia, digits = 2)
 new_name_vector <- c(name_vector[1:7], "E1", "E2")
@@ -65,9 +65,9 @@ save(frame_ginia, file="reports/DNK/tables/frame_ginia.Rda")
 # Mean
 
 frame_meana <- data.frame(year, i11_mean[2], i12_mean[2], i13_mean[2],
-                         i21_mean[2], i22_mean[2], i23_mean[2],
-                         eurostat_mean, row.names = NULL
-                         )
+                          i21_mean[2], i22_mean[2], i23_mean[2],
+                          eurostat_mean, row.names = NULL
+)
 
 frame_meana <- round(frame_meana, digits = 2)
 names(frame_meana) <- name_vector
@@ -77,8 +77,8 @@ save(frame_meana, file="reports/DNK/tables/frame_meana.Rda")
 # Median
 
 frame_mediana <- data.frame(year, i11_median[2], i12_median[2], i13_median[2],
-                         i21_median[2], i22_median[2], i23_median[2],
-                         eurostat_median, row.names = NULL
+                            i21_median[2], i22_median[2], i23_median[2],
+                            eurostat_median, row.names = NULL
 )
 frame_mediana <- round(frame_mediana, digits = 2)
 names(frame_mediana) <- name_vector
@@ -88,9 +88,9 @@ save(frame_mediana, file="reports/DNK/tables/frame_mediana.Rda")
 # Top ten share
 
 frame_t10sa <- data.frame(year, i11_t10s[1], i12_t10s[1], i13_t10s[1],
-                           i21_t10s[1], i22_t10s[1], i23_t10s[1], 
+                          i21_t10s[1], i22_t10s[1], i23_t10s[1], 
                           round(eurostat_t10s/100, digits = 2), row.names = NULL
-                          )
+)
 frame_t10sa <- round(frame_t10sa, digits = 2)
 names(frame_t10sa) <- name_vector
 save(frame_t10sa, file="reports/DNK/tables/frame_t10sa.Rda")
