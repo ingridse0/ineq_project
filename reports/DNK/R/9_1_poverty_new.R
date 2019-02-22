1a46c04dbe7e007f0ab63685662fb42a8d304421
danros1123 <danyell@gmx.net>
2019-02-22 06:33:28 +0100

    no real changes

--- reports/DNK/R/9_1_poverty_new.R
+++ reports/DNK/R/9_1_poverty_new.R
@@ -90,18 +90,18 @@ share_female_over64.p1 <- sum(subset(silc.p1.15,
 # Share male over 64
 
 share_male_over64.p1 <- sum(subset(silc.p1.15,
-                              gender == "Male" & silc.p1.15$arp == 1
-                              & age > 64)$rb050) /
+                                   gender == "Male" & silc.p1.15$arp == 1
+                                   & age > 64)$rb050) /
   sum(subset(silc.p1.15, gender == "Male" & age > 64)$rb050)
 
 # Plot
 
 Altersgruppe <- c("Gesamt", "Gesamt",
-                "Unter 18", "Unter 18",
-                "18-24", "18-24",
-                "25-49", "25-49",
-                "50-64", "50-64",
-                "Über 64", "Über 64")
+                  "Unter 18", "Unter 18",
+                  "18-24", "18-24",
+                  "25-49", "25-49",
+                  "50-64", "50-64",
+                  "Über 64", "Über 64")
 
 Geschlecht <- c("Männlich", "Weiblich",
                 "Männlich", "Weiblich",
@@ -113,11 +113,11 @@ Geschlecht <- c("Männlich", "Weiblich",
 
 
 Armutsgefaehrdungsquote = c(share_male.p1, share_female.p1,
-                           share_male_under18.p1, share_female_under18.p1,
-                           share_male_1924.p1, share_female_1924.p1,
-                           share_male_2549.p1, share_female_2549.p1,
-                           share_male_5064.p1, share_female_5064.p1,
-                           share_male_over64.p1, share_female_over64.p1)
+                            share_male_under18.p1, share_female_under18.p1,
+                            share_male_1924.p1, share_female_1924.p1,
+                            share_male_2549.p1, share_female_2549.p1,
+                            share_male_5064.p1, share_female_5064.p1,
+                            share_male_over64.p1, share_female_over64.p1)
 Armutsgefaehrdungsquote = round(Armutsgefaehrdungsquote * 100, digits = 1)
 
 id = c(1:12)
@@ -162,71 +162,71 @@ silc.p2.15$arp <- ifelse(silc.p2.15$i23 < threshold.p2, 1, 0)
 
 # Share female
 share_female.p2 <- sum(subset(silc.p2.15,
-                           gender == "Female" & silc.p2.15$arp == 1)$rb050) /
+                              gender == "Female" & silc.p2.15$arp == 1)$rb050) /
   sum(subset(silc.p2.15, gender == "Female")$rb050)
 
 # Share male
 share_male.p2 <- sum(subset(silc.p2.15,
-                         gender == "Male" & silc.p2.15$arp == 1)$rb050) /
+                            gender == "Male" & silc.p2.15$arp == 1)$rb050) /
   sum(subset(silc.p2.15, gender == "Male")$rb050)
 
 
 # Share female under 18
 share_female_under18.p2 <- sum(subset(silc.p2.15,
-                                   gender == "Female" & silc.p2.15$arp == 1 & age < 18)$rb050) /
+                                      gender == "Female" & silc.p2.15$arp == 1 & age < 18)$rb050) /
   sum(subset(silc.p2.15, gender == "Female" & age <= 18)$rb050)
 
 # Share male under 18
 share_male_under18.p2 <- sum(subset(silc.p2.15,
-                                 gender == "Male" & silc.p2.15$arp == 1 & age < 18)$rb050) /
+                                    gender == "Male" & silc.p2.15$arp == 1 & age < 18)$rb050) /
   sum(subset(silc.p2.15, gender == "Male" & age <= 18)$rb050)
 
 # Share female 19-24
 share_female_1924.p2 <- sum(subset(silc.p2.15,
-                                gender == "Female" & silc.p2.15$arp == 1
-                                & age >= 18 & age <= 24)$rb050) /
+                                   gender == "Female" & silc.p2.15$arp == 1
+                                   & age >= 18 & age <= 24)$rb050) /
   sum(subset(silc.p2.15, gender == "Female" & age >= 18 & age <=24)$rb050)
 
 # Share male 19-24
 share_male_1924.p2 <- sum(subset(silc.p2.15,
-                              gender == "Male" & silc.p2.15$arp == 1
-                              & age >= 18 & age <= 24)$rb050) /
+                                 gender == "Male" & silc.p2.15$arp == 1
+                                 & age >= 18 & age <= 24)$rb050) /
   sum(subset(silc.p2.15, gender == "Male" & age >= 18 & age <=24)$rb050)
 
 # Share female  25-49
 share_female_2549.p2 <- sum(subset(silc.p2.15,
-                                gender == "Female" & silc.p2.15$arp == 1
-                                & age >= 25 & age <= 49)$rb050) /
+                                   gender == "Female" & silc.p2.15$arp == 1
+                                   & age >= 25 & age <= 49)$rb050) /
   sum(subset(silc.p2.15, gender == "Female" & age >= 25 & age <=49)$rb050)
 
 # Share male 25-49
 share_male_2549.p2 <- sum(subset(silc.p2.15,
-                              gender == "Male" & silc.p2.15$arp == 1
-                              & age >= 25 & age <= 49)$rb050) /
+                                 gender == "Male" & silc.p2.15$arp == 1
+                                 & age >= 25 & age <= 49)$rb050) /
   sum(subset(silc.p2.15, gender == "Male" & age >= 25 & age <=49)$rb050)
 
 # Share female  50-64
 share_female_5064.p2 <- sum(subset(silc.p2.15,
-                                gender == "Female" & silc.p2.15$arp == 1
-                                & age >= 50 & age <= 64)$rb050) /
+                                   gender == "Female" & silc.p2.15$arp == 1
+                                   & age >= 50 & age <= 64)$rb050) /
   sum(subset(silc.p2.15, gender == "Female" & age >= 50 & age <=64)$rb050)
 
 # Share male 50-64
 share_male_5064.p2 <- sum(subset(silc.p2.15,
-                              gender == "Male" & silc.p2.15$arp == 1
-                              & age >= 50 & age <= 64)$rb050) /
+                                 gender == "Male" & silc.p2.15$arp == 1
+                                 & age >= 50 & age <= 64)$rb050) /
   sum(subset(silc.p2.15, gender == "Male" & age >= 50 & age <=64)$rb050)
 
 # Share female over 64
 share_female_over64.p2 <- sum(subset(silc.p2.15,
-                                  gender == "Female" & silc.p2.15$arp == 1
-                                  & age > 64)$rb050) /
+                                     gender == "Female" & silc.p2.15$arp == 1
+                                     & age > 64)$rb050) /
   sum(subset(silc.p2.15, gender == "Female" & age > 64)$rb050)
 
 # Share male over 64
 share_male_over64.p2 <- sum(subset(silc.p2.15,
-                                gender == "Male" & silc.p2.15$arp == 1
-                                & age > 64)$rb050) /
+                                   gender == "Male" & silc.p2.15$arp == 1
+                                   & age > 64)$rb050) /
   sum(subset(silc.p2.15, gender == "Male" & age > 64)$rb050)
 
 ##### Eurostat
